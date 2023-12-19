15843e03c20b78299a7b9600dfaf907a05731bb8
ichikawa <kka78108@gmail.com>
2023-12-20 01:28:09 +0900

    te

--- mypkg/listener.py
+++ mypkg/listener.py
@@ -3,12 +3,19 @@ from rclpy.node import Node
 from std_msgs.msg import Int16
 
 def cb(msg):
+
     global node
-    time = 10000 - msg.data
-    node.get_logger().info("Listen: %d" % time)
+    
+    if msg.data >= 1500:
+
+        node.get_logger().info('1500に到達しました %d' % msg.data)
+
+    else:
+
+        time = 2000 - msg.data
+        node.get_logger().info("Listen: %d" % time)
 
 rclpy.init()
 node = Node("listener")
 pub = node.create_subscription(Int16, "countup", cb,  10)
 rclpy.spin(node)
-
