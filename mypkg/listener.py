6a06ea99c2e5a9d53b264b02eb00d5bf828387aa
ichikawa <kka78108@gmail.com>
2023-12-19 23:27:59 +0900

    test

--- mypkg/listener.py
+++ mypkg/listener.py
@@ -4,9 +4,11 @@ from std_msgs.msg import Int16
 
 def cb(msg):
     global node
-    node.get_logger().info("Listen: %d" % msg.data)
+    time = 10000 - msg.data
+    node.get_logger().info("Listen: %d" % time)
 
 rclpy.init()
 node = Node("listener")
 pub = node.create_subscription(Int16, "countup", cb,  10)
 rclpy.spin(node)
+
