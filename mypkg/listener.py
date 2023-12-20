da4eceb45132284942239c1ea298d9279d98dea2
ichikawa <kka78108@gmail.com>
2023-12-20 15:33:28 +0900

    test

--- mypkg/listener.py
+++ mypkg/listener.py
@@ -5,17 +5,16 @@ from std_msgs.msg import Int16
 def cb(msg):
 
     global node
-    
-    if msg.data >= 1800:
+    time = 2000 - msg.data
 
-        node.get_logger().info('1800に到達しました %d' % msg.data)
+    if time % 5 == 0:
 
-    else:
+        node.get_logger().info('残り %d です' % time)
 
-        time = 2000 - msg.data
-        node.get_logger().info("Listen: %d" % time)
+
+    node.get_logger().info("Listen: %d" % time)
 
 rclpy.init()
 node = Node("listener")
 pub = node.create_subscription(Int16, "countup", cb,  10)
-rclpy.spin(node)git push origin main
+rclpy.spin(node)
