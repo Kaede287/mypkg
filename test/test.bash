284088dbc9c5c8625655d66c8fbe36f57ce1eec7
ichikawa <kka78108@gmail.com>
2023-12-24 14:11:52 +0900

    add test]

--- test/test.bash
+++ test/test.bash
@@ -7,8 +7,20 @@ dir=~
 
 cd $dir/ros2_ws
 colcon build
+
 source $dir/.bashrc
-timeout 15 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
+
+timeout 5 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
+
+cat /tmp/mypkg.log |
+grep 'Listen: 1995 奇数です'
+
+timeout 10 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
 
 cat /tmp/mypkg.log |
 grep 'Listen: 1990 偶数です'
+
+timeout 15 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
+
+cat /tmp/mypkg.log |
+grep '残り 1985 です'
